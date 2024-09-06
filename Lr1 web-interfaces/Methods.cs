eb2d8cc3c5e3c7f9bffb9fab03e351e2d81cfc10
Zatiria <yana02trofimenko@gmail.com>
2024-09-06 17:03:21 +0300

    Добавьте файлы проекта.

---
+++ Lr1 web-interfaces/Methods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lr1_web_interfaces
+{
+    internal class Methods
+    {
+        public void GetWords()
+        {
+            Console.Write("Введіть кількість слів: ");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            using StreamReader reader = new("Lorem Ipsum.txt");
+            String text = reader.ReadToEnd();
+            String[] words = text.Split(' ');
+            if (count > words.Length)
+            {
+                Console.WriteLine("Кількість слів перевищує кількість слів у тексті");
+            }
+            Console.WriteLine(String.Join(" ", words.Take(count)));
+        }
+
+        public void Calculate()
+        {
+            Console.Write("Введіть перше число: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введіть друге число: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введіть операцію (+, -, *, /): ");
+            String operation = Console.ReadLine();
+            String res = "";
+            switch (operation)
+            {
+                case "+":
+                    res = (a + b).ToString();
+                    break;
+                case "-":
+                    res = (a - b).ToString();
+                    break;
+                case "*":
+                    res = (a * b).ToString();
+                    break;
+                case "/":
+                    res = (a / b).ToString();
+                    break;
+                default:
+                    res =  "Невірна операція";
+                    break;
+            }
+            Console.WriteLine("Результат: " + res);
+        }
+    }
+}
---
+++ Lr1 web-interfaces/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lr1_web_interfaces;
+
+Console.WriteLine("Виберіть пункт меню:");
+Console.WriteLine("1. Вивести вказану кількість слів з тексту");
+Console.WriteLine("2. Виконати математичну операцію");
+
+Console.Write("Ваш вибір: ");
+String choise = Console.ReadLine();
+
+Methods methods = new Methods();
+
+switch (choise)
+{
+    case "1":
+        methods.GetWords();
+        break;
+    case "2":
+        methods.Calculate();
+        break;
+    default:
+        Console.WriteLine("Невірний вибір");
+        break;
+}
